ef0f9e60a81a0ec5a4e6154310508b86c470a6b7
insanepartyguy <36977745+insanepartyguy@users.noreply.github.com>
2020-12-26 13:08:40 -0500

    added temperature conversion to F

--- thingspeak_dht22.py
+++ thingspeak_dht22.py
@@ -6,14 +6,12 @@ import time
 import requests
 #Set DATA pin
 DHT = 26
-#while True:
 #Read Temp and Hum from DHT22
-h,t = dht.read_retry(dht.DHT22, DHT)
-#Print Temperature
-print('{0:0.1f}'.format(t,h))
-#Print Humidity
-print('{1:0.1f}'.format(t,h))
+h,t = dht.read_retry(dht.DHT22,DHT)
+h_round = round(h, 1)
+t_f = round((t * 9.0 / 5.0 + 32.0),1)
+#print(h_round,t_f)
 #send data to thingspeak via GET request
 url = 'http://api.thingspeak.com/update.json'
-myobj = {'api_key': 'REDACTED','field6': '{0:0.1f}'.format(t,h),'field5': '{1:0.1f}'.format(t,h)}
+myobj = {'api_key': 'REDACTED','field6': '{0:0.1f}'.format(t_f,h_round),'field5': '{1:0.1f}'.format(t_f,h_round)}
 x = requests.get(url, data = myobj)
